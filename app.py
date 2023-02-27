172b5ac956799dc476c6d532724a683e91187495
Roshani Srivastava <105964925+RoshaniSrivastava@users.noreply.github.com>
2023-02-28 01:52:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("A Fashion Designer Who Transitted her Career to Data Science")
+
+st.header("Wanna Know How? :astonished:")
+
+bttn_click = st.button("Click Here :blush:")
+
+if bttn_click == True:
+    st.subheader("So, lemme start introducing myself I am Roshani Srivastava (Fashion designer and Data Analyst).Now coming from a non-tech domain it was quite challenging for me to learn programming and other technical stuffs but I know the fact tha I am outgoing, dedicated, and open-minded. I get across to people and adjust to changes with ease.Seeking a role to enhance and explore my technical knowledge gained at IIIT-B in the Data Science domain.3+ years of fashion designing and fashion analytics experience.I know computing languages like python, MS Excel, MySQL, Machine Learning, and visualization tools like Tableau and Power BI, and have certification in this from IIIT- B and UpGrad.Currently, I am looking for best career oppurtunities in data science roles.")
+    st.balloons()
+    
+
+
+
+
